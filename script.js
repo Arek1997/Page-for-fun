716df09b63a6e4094e3f1f3476173dc8af86c4f5
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2022-04-14 10:01:44 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const btnHeader = document.getElementById("btnPortfolio");
+const sectionPortfolio = document.querySelector(".portfolio");
+const allSections = document.querySelectorAll(".section");
+
+// Button smooth scroll
+btnHeader.addEventListener("click", function () {
+  sectionPortfolio.scrollIntoView({ behavior: "smooth" });
+});
+
+// RevealSections
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  // If Intersecting is false
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+// Set observe for all section and add hidden class
+
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
